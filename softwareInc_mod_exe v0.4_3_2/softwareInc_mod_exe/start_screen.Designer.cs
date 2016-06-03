16e9cee13eee99ecae2797bfc4200b84b68164ce
Squalalah <riquezoune@gmail.com>
2016-06-03 21:27:50 +0200

    Rename button start screen + add french
    Rename button1,2,3 to button_it (italian), de (deutsch), en (english),
    fr (french) + add french button.

--- softwareInc_mod_exe v0.4_3_2/softwareInc_mod_exe/start_screen.Designer.cs
+++ softwareInc_mod_exe v0.4_3_2/softwareInc_mod_exe/start_screen.Designer.cs
@@ -30,9 +30,10 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(start_screen));
             this.label1 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
+            this.button_it = new System.Windows.Forms.Button();
+            this.button_en = new System.Windows.Forms.Button();
+            this.button_de = new System.Windows.Forms.Button();
+            this.button_fr = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -40,51 +41,64 @@
             this.label1.AutoSize = true;
             this.label1.BackColor = System.Drawing.Color.Transparent;
             this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.label1.Font = new System.Drawing.Font("Hobo Std", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.Location = new System.Drawing.Point(41, 98);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(721, 34);
+            this.label1.Size = new System.Drawing.Size(722, 29);
             this.label1.TabIndex = 0;
             this.label1.Text = "Welcome to my Modding Program for Software Inc. the Game";
             // 
-            // button1
+            // button_it
             // 
-            this.button1.BackgroundImage = global::softwareInc_mod_exe.Properties.Resources.fahne_italienisch;
-            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.button1.Font = new System.Drawing.Font("Hobo Std", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(12, 242);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(242, 285);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "Italiano";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button_it.BackgroundImage = global::softwareInc_mod_exe.Properties.Resources.fahne_italienisch;
+            this.button_it.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button_it.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_it.Location = new System.Drawing.Point(12, 338);
+            this.button_it.Name = "button_it";
+            this.button_it.Size = new System.Drawing.Size(189, 189);
+            this.button_it.TabIndex = 1;
+            this.button_it.Text = "Italiano";
+            this.button_it.UseVisualStyleBackColor = true;
+            this.button_it.Click += new System.EventHandler(this.button_it_Click);
             // 
-            // button2
+            // button_en
             // 
-            this.button2.BackgroundImage = global::softwareInc_mod_exe.Properties.Resources.England_flag_5;
-            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.button2.Font = new System.Drawing.Font("Hobo Std", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(286, 242);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(242, 285);
-            this.button2.TabIndex = 2;
-            this.button2.Text = "English";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.button_en.BackgroundImage = global::softwareInc_mod_exe.Properties.Resources.England_flag_5;
+            this.button_en.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button_en.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_en.Location = new System.Drawing.Point(208, 338);
+            this.button_en.Name = "button_en";
+            this.button_en.Size = new System.Drawing.Size(189, 189);
+            this.button_en.TabIndex = 2;
+            this.button_en.Text = "English";
+            this.button_en.UseVisualStyleBackColor = true;
+            this.button_en.Click += new System.EventHandler(this.button_en_Click);
             // 
-            // button3
+            // button_de
             // 
-            this.button3.BackgroundImage = global::softwareInc_mod_exe.Properties.Resources.HD_German_Flag_Wallpaper_300x168;
-            this.button3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.button3.Font = new System.Drawing.Font("Hobo Std", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button3.Location = new System.Drawing.Point(563, 242);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(242, 285);
-            this.button3.TabIndex = 3;
-            this.button3.Text = "Deutsch";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
+            this.button_de.BackgroundImage = global::softwareInc_mod_exe.Properties.Resources.HD_German_Flag_Wallpaper_300x168;
+            this.button_de.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button_de.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_de.Location = new System.Drawing.Point(412, 338);
+            this.button_de.Name = "button_de";
+            this.button_de.Size = new System.Drawing.Size(189, 189);
+            this.button_de.TabIndex = 3;
+            this.button_de.Text = "Deutsch";
+            this.button_de.UseVisualStyleBackColor = true;
+            this.button_de.Click += new System.EventHandler(this.button_de_Click);
+            // 
+            // button_fr
+            // 
+            this.button_fr.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button_fr.BackgroundImage")));
+            this.button_fr.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button_fr.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_fr.Location = new System.Drawing.Point(616, 338);
+            this.button_fr.Name = "button_fr";
+            this.button_fr.Size = new System.Drawing.Size(189, 189);
+            this.button_fr.TabIndex = 4;
+            this.button_fr.Text = "Français";
+            this.button_fr.UseVisualStyleBackColor = true;
+            this.button_fr.Click += new System.EventHandler(this.button_fr_Click);
             // 
             // start_screen
             // 
@@ -93,13 +107,15 @@
             this.BackgroundImage = global::softwareInc_mod_exe.Properties.Resources._2015_06_09_00001;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(817, 539);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button_fr);
+            this.Controls.Add(this.button_de);
+            this.Controls.Add(this.button_en);
+            this.Controls.Add(this.button_it);
             this.Controls.Add(this.label1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "start_screen";
             this.Text = "Software Inc. Modding Program by amensch";
+            this.Load += new System.EventHandler(this.start_screen_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -108,8 +124,9 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button_it;
+        private System.Windows.Forms.Button button_en;
+        private System.Windows.Forms.Button button_de;
+        private System.Windows.Forms.Button button_fr;
     }
 }
